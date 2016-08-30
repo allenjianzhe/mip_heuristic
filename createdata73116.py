bcea6240886282372ebb05e2040b6181c306aefc
Allen <allenjianzhe@yahoo.com>
2016-08-30 09:29:32 -0400

    commit msg

--- createdata73116.py
+++ createdata73116.py
@@ -11,8 +11,9 @@ def generate_cutomer_data(size_of_customer):
     data = pd.DataFrame(
         columns=['Customer_Id', 'Final_Destination', 'Demand', 'Due_Date', 'Penalty'])
     for i in range(size_of_customer):
-        data.loc[i] = [i, 'SS', uniform_dist(50, 250), uniform_dist(6, 35), 50]
+        # data.loc[i] = [i, 'SS', uniform_dist(50, 250), uniform_dist(6, 35), 50]
+        data.loc[i] = [i, 'SS', uniform_dist(50, 250), uniform_dist(11, 65), 50]
     return data
-size_of_customer = 5
+size_of_customer = 20
 d = generate_cutomer_data(size_of_customer)
-d.to_csv('C:\Users\MacBook Air\Desktop\my research\cus_5_14.csv', index=False)
+d.to_csv('C:\Users\MacBook Air\Desktop\my research\cus_20_15.csv', index=False)
